1a69c62de4bd75aec9215ac5a19a72892cbcc042
Digant Vyas <96488428+dmvyas@users.noreply.github.com>
2024-09-26 11:36:19 +0000

    pushing what learnt

---
+++ 02_variables/var.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "pratik@js.com"
+var accountPassword = "232323"
+accountCity = "Jaipur"
+
+
+// accountId = 23 not allowd
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
---
+++ 03_data type/d.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version 
+/*alert("hi") - this will not work because we are in node 
+to do this we have different package that we will learn after*/ 
+console.log("answer of 3+3:");
+console.log(3+3);
+
+// number => 2 to power 53
+// bigint
+// string => "!I
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console. log(typeof undefined); // undefined
+console. log(typeof null); // object
